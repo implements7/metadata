8e5469e262aa84d5d95da5e8a7dab2fa22e3600a
John Squibb <admin@johnsquibb.com>
2020-03-21 16:09:44 -0700

    Breaking examples for registry magicals into own file

--- examples/registryExample.php
+++ examples/registryExample.php
@@ -33,26 +33,3 @@ $name = 'TrackMe';
 $meta->register($myVar, $box, $name);
 $meta->getByName($name)->getItemByName('counters')->hit('called');
 var_dump(['named registration' => $meta->getByName($name)->getPackage()]);
-
-// For convenience, objects registered by name can use ArrayAccess via registry.
-$meta[$name]->getItemByName('counters')->hit('called');
-var_dump(['named registration (ArrayAccess)' => $meta[$name]->getPackage()]);
-
-// The box also supports array access, so we can do this.
-$meta[$name]['counters']->hit('called');
-$meta[$name]['counters']->hit('called');
-var_dump(['counters (ArrayAccess)' => $meta[$name]->getPackage()]);
-
-// If the added items support ActionItemInterface, then we can call them as functions.
-$meta[$name]['counters']('called');
-$meta[$name]['counters']('called');
-$meta[$name]['counters']('called');
-
-var_dump(['counters as ActionItemInterface (ArrayAccess)' => $meta[$name]->getPackage()]);
-
-// Like array access, object access is also available by calling the function on a member.
-($meta->TrackMe->counters)('ObjectAccessInvoked');
-// The above example is awkward, so method call is also supported.
-$meta->TrackMe->counters('ObjectAccessInvoked');
-
-var_dump(['counters as ObjectAccess' => $meta[$name]->getPackage()]);
